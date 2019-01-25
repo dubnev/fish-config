e81ff5a7893757aa1368fa45a74f853bc5c702df
Will Neville <william@neville.com>
2019-01-25 09:54:26 -0500

    making config.fish a hard link so I can reference it in this repo

--- config.fish
+++ config.fish
@@ -1,1 +1,19 @@
-/Users/will/.config/fish/config.fish
+# Initialize pyenv, suppress output
+eval 'pyenv init -' > /dev/null
+
+# Source custom fish functions
+for file in ~/.fish/fish_funcs/*
+    source $file
+end
+
+# Source other fish configuration
+for file in ~/.fish/fish_config/*
+    source $file
+end
+
+# Update PATH for gcloud SDK
+bass source '/Applications/google-cloud-sdk/path.bash.inc'
+
+# Shell command completion for gcloud cli
+bass source '/Applications/google-cloud-sdk/completion.bash.inc'
+
